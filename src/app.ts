ee0c9e0dae88dd89070224aec341c0bc68245411
Felipe Figueroa Vergara <felipeafigueroav@gmail.com>
2021-05-02 19:09:20 -0400

    se añade middleware cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { send } from "node:process";
 import "./create-database";
+import cors from "cors";
 import {router as login_router } from "./routes/login_routes";
 const app = express();
 const port = process.env.port || 3000;
 
 //global middleware
+/**Middleware for cors policy*/
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:4200'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded());
