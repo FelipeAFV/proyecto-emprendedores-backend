eb841d40fe1d8bc221173402586801ecf394bc54
Jose <jose.vargas.t@csi.cl>
2021-05-02 23:18:51 -0400

    cambio

--- src/create-database.ts
+++ src/create-database.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import * as helmet from 'helmet';
 
 createConnection().then(() => {
     console.log('Connection to database successful')
