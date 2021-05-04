d90ef30ac8e59a3a879ac00140a8c0e441bf4360
Jose <jose.vargas.t@csi.cl>
2021-05-04 14:15:02 -0400

    remove

--- src/middlewares/role-auth.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response, NextFunction} from 'express';
-import UserService from '../services/user-service';
-
-class RoleAuthMid {
-    
-    
-}
-
-export default new RoleAuthMid();
