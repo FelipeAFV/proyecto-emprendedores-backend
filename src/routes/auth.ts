ea9ddc847c74b18ff509f2ab6808258423838a82
Jose <jose.vargas.t@csi.cl>
2021-06-05 13:10:13 -0400

    agregando middleware a client routes y unaas correcciones

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,5 @@
 import {Router,Request,Response} from "express";
 import authController from "../controllers/auth.controller";
-import clientController from "../controllers/client.controller";
-import ClientController from "../controllers/client.controller";
 import cookie_check from "../middlewares/payload_checker";
 const router : Router = Router();
 
@@ -11,6 +9,6 @@ router.get("/logout",authController.logout)
 router.get("/isLogged",authController.isLogged)
 router.post("/signIn", authController.signIn)
 router.post("/signUp",authController.signUp)
-router.post("/test",clientController.getFavoritesStores)
+
 
 export {router};
--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -1,9 +1,10 @@
 import {Router,Request,Response} from "express";
+import { AppRole } from "model/enums/app-role";
 import clientController from "../controllers/client.controller";
+import RoleAuth from "../middlewares/role-auth";
 const router : Router = Router();
 
-router.get("/favoriteStores",clientController.getFavoritesStores);
-router.put('/')
-router.delete('/favoritesStores/:id', clientController.deleteFavoriteStore);
+router.get("/favoriteStores",RoleAuth.checkRole([AppRole.CLIENT]),clientController.getFavoritesStores);
+router.delete('/favoritesStores/:id',RoleAuth.checkRole([AppRole.CLIENT]), clientController.deleteFavoriteStore);
 
 export default router;
