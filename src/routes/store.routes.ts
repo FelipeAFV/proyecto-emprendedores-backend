fa17a296bb33d649e80f83c2ea30ffd5a799b7e5
unknown <samuelborraccini@hotmail.com>
2021-06-05 15:28:01 -0400

    añadida proteccion de rutas

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import storeController from "../controllers/store.controller";
+import RoleAuth from "../middlewares/role-auth";
+import { AppRole } from "model/enums/app-role";
 const router: Router = Router();
 
-router.post('/', storeController.createStore);
+router.post('/', RoleAuth.checkRole([AppRole.ADMIN,AppRole.STORE_MANAGER]),storeController.createStore);
 router.get('/:storeName', storeController.getStoreByName);
 
 export default router;
