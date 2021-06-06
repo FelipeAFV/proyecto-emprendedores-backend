f14daefc6f3648ddef533396dfe7d227134ae12a
unknown <samuelborraccini@hotmail.com>
2021-06-06 13:04:53 -0400

    añadida proteccion de rutas checkstores

--- src/routes/authorization.routes.ts
+++ src/routes/authorization.routes.ts
@@ -1,7 +1,9 @@
 import {Router} from "express";
 import authorizationController from "../controllers/authorization.controller";
+import RoleAuth from "../middlewares/role-auth";
+import { AppRole } from "model/enums/app-role";
 const router = Router();
 
-router.post('/isStoreOwner', authorizationController.isStoreOwner);
+router.post('/isStoreOwner', RoleAuth.checkRole([AppRole.STORE_MANAGER]),authorizationController.isStoreOwner);
 
 export default router;
